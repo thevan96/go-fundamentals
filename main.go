42bd8d29c11fd416f9baed4ad67392afacd3e3b2
thevan96 <vothevan96@gmail.com>
2026-09-20 20:49:01 +0700

    done

--- main.go
+++ main.go
@@ -5,14 +5,9 @@ import "fmt"
 func main() {
 	var n int
 	fmt.Scan(&n)
-	switch {
-	case n%15 == 0:
-		fmt.Println("FizzBuzz")
-	case n%3 == 0:
-		fmt.Println("Fizz")
-	case n%5 == 0:
-		fmt.Println("Buzz")
-	default:
-		fmt.Println(n)
+	var sum int
+	for i := 1; i <= n; i++ {
+		sum += i
 	}
+	fmt.Println(sum)
 }
