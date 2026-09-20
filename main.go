59e81b78a53c67023b524a91571aab4a23f27079
thevan96 <vothevan96@gmail.com>
2026-09-20 21:17:17 +0700

    done

--- main.go
+++ main.go
@@ -2,12 +2,12 @@ package main
 
 import "fmt"
 
+func square(n int) int {
+	return n * n
+}
+
 func main() {
 	var n int
 	fmt.Scan(&n)
-	var sum int
-	for i := 1; i <= n; i++ {
-		sum += i
-	}
-	fmt.Println(sum)
+	fmt.Println(square(n))
 }
