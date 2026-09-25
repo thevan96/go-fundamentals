65ba7ae5d78347c234c0d9ab79dcf900ccb4afc9
thevan96 <vothevan96@gmail.com>
2026-09-25 16:51:02 +0700

    done

--- main.go
+++ main.go
@@ -1,25 +1,33 @@
 package main
 
-import "fmt"
-
-type Point struct {
-	X, Y int
-}
-
-func Distance(p1, p2 Point) int {
-	deltaX := (p1.X -p2.X)
-	deltaY := (p1.Y -p2.Y)
-	return deltaX*deltaX + deltaY *deltaY
-}
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
 
 func main() {
-	var x1, y1, x2, y2 int
-	fmt.Scan(&x1)
-	fmt.Scan(&y1)
-	fmt.Scan(&x2)
-	fmt.Scan(&y2)
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Split(bufio.ScanWords)
+	var nums []int
+	for scanner.Scan() {
+		num, _ := strconv.Atoi(scanner.Text())
+		nums = append(nums, num)
+		if scanner.Err() != nil {
+			break
+		}
+	}
 
-	p1 := Point{x1, y1}
-	p2 := Point{x2, y2}
-	fmt.Println(Distance(p1, p2))
+	for i := range nums {
+		increment(&nums[i])
+	}
+
+	for _, num := range nums {
+		fmt.Println(num)
+	}
+}
+
+func increment(n *int) {
+	*n++
 }
