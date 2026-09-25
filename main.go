4b36196b51dd2e317ece5d69c303de217ba7f59a
thevan96 <vothevan96@gmail.com>
2026-09-25 16:03:22 +0700

    done

--- main.go
+++ main.go
@@ -1,28 +1,25 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
+
+type Point struct {
+	X, Y int
+}
+
+func Distance(p1, p2 Point) int {
+	deltaX := (p1.X -p2.X)
+	deltaY := (p1.Y -p2.Y)
+	return deltaX*deltaX + deltaY *deltaY
+}
 
 func main() {
-	frequencies := map[string]int{}
-	var word string
+	var x1, y1, x2, y2 int
+	fmt.Scan(&x1)
+	fmt.Scan(&y1)
+	fmt.Scan(&x2)
+	fmt.Scan(&y2)
 
-	for {
-		_, err := fmt.Scan(&word)
-		if err != nil {
-			break
-		}
-		frequencies[word]++
-	}
-
-	different := len(frequencies)
-	once := 0
-	for _, v := range frequencies {
-		if v == 1 {
-			once++
-		}
-	}
-	fmt.Println(different)
-	fmt.Println(once)
+	p1 := Point{x1, y1}
+	p2 := Point{x2, y2}
+	fmt.Println(Distance(p1, p2))
 }
